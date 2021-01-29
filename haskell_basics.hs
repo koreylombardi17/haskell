0b8cd587422748521c656175c12b25be4f39e64a
Korey Lombardi <koreylombardi17@yahoo.com>
2021-01-29 00:19:10 -0500

    haskell basics

--- haskell_basics.hs
+++ haskell_basics.hs
@@ -1,10 +1,7 @@
--- Korey Lombardi 
--- 1/28/2021
--- COP 4020 : Homework 1
 
-module Hw1 where
+module Haskell_basics where
 
--- 1. Fix the syntax errors in the program below, and test your solution using GHCi.
+-- Fix the syntax errors in the program below, and test your solution using GHCi.
 -- N = a 'div' length xs 
 --     where
 --        a = 10
@@ -21,19 +18,17 @@ divideByListSize n = a `div` length xs
     xs = [1,2,3,4,5]
 
 
--- 2. Show how the library function last that selects the last element of a list can be defined using the
---    functions introduced in this lecture. For example:
---      > last [1,2,3,4,5]
---      > 5
+-- Show how the library function last that selects the last element of a list can be defined using basic functions
+-- > last [1,2,3,4,5]
+-- > 5
 --
 -- Implementation below:
 -- head (reverse [1,2,3,4,5])
 
 
--- 3. Show how the library function init that removes the last element from a list can be defined. For
---    example:
---             > init [1,2,3,4,5]
---             > [1,2,3,4]
+-- Show how the library function init that removes the last element from a list can be defined. For example:
+-- > init [1,2,3,4,5]
+-- > [1,2,3,4]
 
 init' :: [Int] -> [Int]
 init' [] = []
@@ -41,13 +36,13 @@ init' [x] = []
 init' (x:xs) = [x] ++ init' xs
 
 
--- 4. What are the types of the following values?
+-- What are the types of the following values?
 --    [’a’,’b’,’c’] = [Char]
 --    (’a’,’b’,’c’) = (Char, Char, Char)
 --    [(False,’0’),(True,’1’)] = [(Bool, Char)]
 
 
--- 5. What are the types of the following functions?
+-- What are the types of the following functions?
 --    second xs = head (tail xs) = [a] -> a
 --    swap (x,y) = (y,x) = (b, a) -> (a, b)
 --    pair x y = (x,y) = a -> b -> (a, b)
