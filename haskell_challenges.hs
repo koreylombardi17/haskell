e3f1f4b245cd8b041a4c21d029c0dca9cc29b127
Korey Lombardi <koreylombardi17@yahoo.com>
2021-02-14 10:42:08 -0500

    Completed challenges

--- haskell_challenges.hs
+++ haskell_challenges.hs
@@ -6,19 +6,19 @@ module Haskell_Challenges where
 -- Example 1
 or1 :: Bool -> Bool -> Bool
 or1 False False = False
-or1 _ _ = True
+or1 _ _         = True
 
 -- Example 2
 or2 :: Bool -> Bool -> Bool
-or2 True _ = True
-or2 _ True = True
+or2 True _      = True
+or2 _ True      = True
 or2 False False = False
 
 -- Example 3
 or3 :: Bool -> Bool -> Bool
-or3 True True = True
-or3 True False = True
-or3 False True = True
+or3 True True   = True
+or3 True False  = True
+or3 False True  = True
 or3 False False = False
 
 
@@ -56,3 +56,65 @@ perfects n = [x | x <- [1..n], (x == sum (get_factors x))]
 -- Using a list comprehension, define a function that returns the scalar product of two lists.
 scalar_prod :: [Int] -> [Int] -> Int
 scalar_prod xs ys = sum [(x * y) | (x, y) <- zip xs ys]
+
+
+-- Without looking at the standard prelude, define the following library functions using recursion:
+
+-- and :: [Bool] -> Bool
+and'' :: [Bool] -> Bool
+and'' []     = True
+and'' (x:xs) = x && and'' xs
+
+
+-- Concatenate a list of lists:
+-- concat :: [[a]] -> [a]
+concat' :: [[a]] -> [a]
+concat' []     = []
+concat' (x:xs) = x ++ concat' xs
+
+
+-- Produce a list with n identical elements:
+-- replicate -> Int -> a -> [a]
+replicate' :: Int -> a -> [a]
+replicate' 0 _ = []
+replicate' n a = a : replicate' (n-1) a
+
+
+-- Select the nth element of a list:
+-- (!!) :: [a] -> Int -> a
+(!!!) :: [a] -> Int -> a
+(!!!) [] _     = error "index too large"
+(!!!) (x:xs) 0 = x
+(!!!) (x:xs) n = (!!!) xs (n-1)
+
+
+-- Decide if a value is an element of a list:
+-- elem :: Eq a => a -> [a] -> Bool
+elem' :: Eq a => a -> [a] -> Bool
+elem' _ [] = False
+elem' y (x:xs) | (y == x) = True
+               | otherwise = elem' y xs
+
+
+-- Define a recursive function
+-- that merges two sorted lists of values to give a single sorted list
+-- merge :: Ord a => [a] -> [a] -> [a]
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] [] = []
+merge xs [] = xs
+merge [] ys = ys
+merge l1@(x:xs) l2@(y:ys)
+  | (x <= y)  = x : merge xs l2
+  | otherwise = y : merge l1 ys
+
+
+-- Define merge sort
+msort :: Ord a => [a] -> [a]
+msort []  = []
+msort [x] = [x]
+msort xs = merge ys zs
+             where 
+               ys     = msort $ take middle xs
+               zs     = msort $ drop middle xs
+               middle = length xs `div` 2
+
